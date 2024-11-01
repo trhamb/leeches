f015ad406625eb2e061e16db56ad4a3af4ee7721
hollowgoose <tom@sightsupportworthing.org.uk>
2024-11-01 14:51:26 +0000

    minor changes

--- index.js
+++ index.js
@@ -488,5 +488,5 @@ app.post("/reports/generate", checkAuth, async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server now running on port ${port}`);
 });
